9ae608760fc64df391bc615920d5a2be6e002c48
basem909 <basemshams30@gmail.com>
2022-10-12 05:18:17 -0300

    Create navbar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Home from "./components/home";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+    <Navbar />
+    <Home />
+      <h1 className="text-5xl">Hello</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/home.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const home = () => {
+  return <div name='' className="w-full h-screen bg-[#0a192f]"></div>;
+}
+
+export default home
