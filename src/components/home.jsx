3d10ebf510877a58964daad3bb7523c71b9ad3ef
basem909 <basemshams30@gmail.com>
2022-10-13 06:31:15 -0300

    Fix link bug

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -18,7 +18,7 @@ const home = () => {
           not hesitate and contact me.
         </p>
         <div>
-          <Link href='/contact' className='text-white px-6 border-2 py-3 my-3 flex items-center hover:bg-pink-600 hover:border-pink-600 rounded-md hover:transition-shadow duration-300 max-w-[200px]'>Start collaboration</Link>
+          <Link to='/contact' className='text-white px-6 border-2 py-3 my-3 flex items-center hover:bg-pink-600 hover:border-pink-600 rounded-md hover:transition-shadow duration-300 max-w-[200px]'>Start collaboration</Link>
         </div>
       </div>
     </div>
