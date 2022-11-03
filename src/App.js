d4693d68e1cc769bb8e27fcdf1b2d633be3691e0
basem909 <basemshams30@gmail.com>
2022-11-03 16:17:31 -0300

    Fix Importing bug

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Contact from "./components/Contact";
 import Home from "./components/home";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
+import RouteChangeTracker from "./components/RouteChangeTracker";
 import Skills from "./components/Skills";
 
 function App() {
